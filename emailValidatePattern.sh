ffb0fc9c26fdfec68b3d9552f25fd3687eed5e9e
roshan1718 <roshanshinde1717@gmail.com>
2020-03-26 10:34:53 +0530

    ADDED code to validate the mandatory part

---
+++ emailValidatePattern.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+shopt -s extglob
+
+# @ Description: Email address validate pattern
+# @ Author: Roshan Balkrushna Shinde
+# @ Version: 18.04.3 lts
+# @ Since: 26 March 2020 
+
+EMAIL_PATTERN="^[a-z]{1,}([.]?[a-z]{1,})?[@]{1}[a-z]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2})?$"
+
+read -p "Enter Email-id " email
+	if [[ $email =~ $EMAIL_PATTERN ]]; then
+   	echo Valid Email-Id
+	else
+   	echo Invalid Email-Id
+	fi
